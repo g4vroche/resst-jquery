6321b4e444af0c36ad760602ffdbad91c10520c7
Hugues Charleux <hcx@uhuru.io>
2016-06-17 16:05:12 +0200

    Import backend

---
+++ index.js
@@ -0,0 +1,60 @@
+
+module.exports = {
+
+    "handle": function(parameters){
+        var params = parameters;
+        params.type = parameters.method;
+        params.cache = true;
+        params.url = parameters.uri;
+        params.data = params.body;
+
+        return new Promise(function(resolve, reject){
+
+            params.success = function(body, status, response){
+                resolve(formatResponse(response));
+            };
+
+            params.error = function(response){
+                reject(formatResponse(response));
+            };
+
+
+            request.ajax(params);
+        });
+    }
+};
+
+
+function formatResponse(response){
+
+    return {
+        "headers": parseHeaders(response.getAllResponseHeaders()),
+        "status": {
+            "code": response.status,
+            "message": response.statusText
+        },
+        "body": response.responseText,
+        "data": formatBody(response)
+    };
+}
+
+function formatBody(response){
+//    var contentType = response.getResponseHeader("Content-type");
+//    if (contentType && contentType.split(";")[0] === "application/json") {
+        return JSON.parse(response.responseText);
+//    }
+}
+
+function parseHeaders(headerSting){
+    var headers = {};
+    headerSting.split("\r\n").map( function(line) {
+        if(line.trim() !== ""){
+            line = line.split(":");
+            headers[line.shift().trim()] = line.join(":").trim();
+        }
+    });
+
+    return headers;
+}
+
+
