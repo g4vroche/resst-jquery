06676d0e3ce3000bfd430ec36e0bdce298dd5f2a
Hugues Charleux <hcx@uhuru.io>
2016-06-28 15:52:52 +0200

    Remove json decoding from backend

--- index.js
+++ index.js
@@ -35,15 +35,7 @@ function formatResponse(response){
             "message": response.statusText
         },
         "body": response.responseText,
-        "data": formatBody(response)
     };
-}
-
-function formatBody(response){
-//    var contentType = response.getResponseHeader("Content-type");
-//    if (contentType && contentType.split(";")[0] === "application/json") {
-        return JSON.parse(response.responseText);
-//    }
 }
 
 function parseHeaders(headerSting){
